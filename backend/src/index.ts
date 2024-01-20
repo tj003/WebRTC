f1717767a78d2a4550da47c19421fae69d81bfe1
Tushar Jadhav <tusharj071@gmai.com>
2024-01-20 19:44:59 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,48 @@
+// import { Socket } from "socket.io";
+// import http from "http";
+
+// import express from 'express';
+// import { Server } from 'socket.io';
+// import { UserManager } from "./managers/UserManger";
+
+// const app = express();
+// const server = http.createServer(http);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*"
+//   }
+// });
+
+// const userManager = new UserManager();
+
+// io.on('connection', (socket: Socket) => {
+//   console.log('a user connected');
+//   userManager.addUser("randomName", socket);
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//     userManager.removeUser(socket.id);
+//   })
+// });
+
+// server.listen(5000, () => {
+//     console.log('listening on *:5000');
+// });
+// // server.listen("https://webrtc-backend-hvws.onrender.com", () => {
+// //     console.log('listening on *:5000');
+// // });
+
+//////////////////////////////////////////////////////////////////////////////
+
 import { Socket } from "socket.io";
 import http from "http";
-
 import express from 'express';
 import { Server } from 'socket.io';
 import { UserManager } from "./managers/UserManger";
+import cors from 'cors';  // Import the cors middleware
 
 const app = express();
-const server = http.createServer(http);
+const server = http.createServer(app);  // Pass the 'app' to createServer
 
 const io = new Server(server, {
   cors: {
@@ -15,6 +51,8 @@ const io = new Server(server, {
 });
 
 const userManager = new UserManager();
+
+app.use(cors());  // Enable CORS for all routes
 
 io.on('connection', (socket: Socket) => {
   console.log('a user connected');
@@ -26,8 +64,5 @@ io.on('connection', (socket: Socket) => {
 });
 
 server.listen(5000, () => {
-    console.log('listening on *:5000');
+  console.log('listening on *:5000');
 });
-// server.listen("https://webrtc-backend-hvws.onrender.com", () => {
-//     console.log('listening on *:5000');
-// });
